bcabacc91597e917de0c4d0a2096dde9eb86270d
creamtic <crocus@kakao.com>
2025-02-03 21:50:08 +0900

    update opg

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,9 +27,12 @@ export default function RootLayout({
     <html lang="ko">
       <Head>
         <link href="https://hangeul.pstatic.net/hangeul_static/css/maru-buri.css" rel="stylesheet" />
-        <meta property="og:title" content="관우 ❤ 세란 결혼합니다." />
+        <meta property="og:title" content="모바일 청첩장" />
         <meta property="og:description" content="관우 ❤ 세란 결혼합니다." />
-        <meta property="og:image" content="https://ca.slack-edge.com/T061BFHLJJ1-U07M25JQXCH-g2780cf16183-512" />
+        <meta
+          property="og:image"
+          content="https://s.pstatic.net/static/www/mobile/edit/2016/0705/mobile_212852414260.png"
+        />
         <meta property="og:url" content="https://kw-sr.vercel.app" />
         <meta property="og:type" content="website" />
 
