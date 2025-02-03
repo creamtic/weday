2cc70bcdbfa25a0a7e2fb0369aafa0fe93b23e19
creamtic <crocus@kakao.com>
2025-02-03 23:59:14 +0900

    add account information

--- src/constants/constant.ts
+++ src/constants/constant.ts
@@ -10,23 +10,23 @@ export const GROOM_ACCOUNT = "신한은행 110-334-866541";
 
 export const GROOM_FATHER_NAME = "고태영";
 export const GROOM_FATHER_MOBILE = "01035075887";
-export const GROOM_FATHER_ACCOUNT = "입력 필요";
+export const GROOM_FATHER_ACCOUNT = "대구은행 508-12-893286-3";
 
 export const GROOM_MOTHER_NAME = "이숙이";
 export const GROOM_MOTHER_MOBILE = "01029759298";
-export const GROOM_MOTHER_ACCOUNT = "입력 필요";
+export const GROOM_MOTHER_ACCOUNT = "대구은행 227-07-008265-001";
 
 export const BRIDE_NAME = "이세란";
 export const BRIDE_MOBILE = "01097273147";
-export const BRIDE_ACCOUNT = "입력 필요";
+export const BRIDE_ACCOUNT = "토스뱅크 1000-0942-9418";
 
 export const BRIDE_FATHER_NAME = "이승구";
 export const BRIDE_FATHER_MOBILE = "01031853147";
-export const BRIDE_FATHER_ACCOUNT = "입력 필요";
+export const BRIDE_FATHER_ACCOUNT = "농협 207056-52-025880";
 
 export const BRIDE_MOTHER_NAME = "조완복";
 export const BRIDE_MOTHER_MOBILE = "01022593147";
-export const BRIDE_MOTHER_ACCOUNT = "입력 필요";
+export const BRIDE_MOTHER_ACCOUNT = "농협 207056-56-022448";
 
 export const T_MAP_URI = "https://tmap.life/ce74a5bc";
 export const KAKAO_MAP_URI = "https://map.kakao.com/?itemId=17893188";
