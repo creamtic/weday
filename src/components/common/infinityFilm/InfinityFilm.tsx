49d227f1e6eaa7fb810142a6cdc0f4f734aae2af
creamtic <crocus@kakao.com>
2025-02-04 23:15:26 +0900

    remove unnecessary packages

--- src/components/common/infinityFilm/InfinityFilm.tsx
+++ src/components/common/infinityFilm/InfinityFilm.tsx
@@ -1,53 +1,5 @@
 import React from "react";
-import { styled } from "styled-components";
-
-const View = styled.div`
-  display: flex;
-  flex-direction: column;
-  padding: 0.5rem;
-  overflow: hidden;
-`;
-
-const Slide = styled.ul`
-  display: flex;
-  position: relative;
-  left: -100px;
-  width: calc(10rem * 14);
-  animation: autoPlay 25s linear infinite;
-
-  @keyframes autoPlay {
-    0% {
-      transition: translateX(0);
-    }
-    100% {
-      transform: translateX(calc(-10rem * 7));
-    }
-  }
-
-  & > li {
-    width: 10rem;
-    list-style: none;
-    margin: 0 0.5rem 0 0.5rem;
-  }
-
-  /* 스타일 추가: 각 이미지를 둥근 필름 형태로 만들기 */
-  & .image-container {
-    width: 100%;
-    height: 100%;
-    aspect-ratio: 1; /* 가로세로 비율을 정사각형으로 설정 */
-    overflow: hidden; /* 이미지가 넘칠 경우 잘라내기 */
-    border-radius: 1rem; /* 둥근 모서리 */
-    box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.3); /* 그림자 */
-    position: relative;
-  }
-
-  & img {
-    width: 100%;
-    height: 100%;
-    object-fit: cover;
-    scale: 1.5;
-  }
-`;
+import "./InfinityFilm.css";
 
 interface InfinityFilmProps {
   images: string[];
@@ -56,18 +8,17 @@ interface InfinityFilmProps {
 
 const InfinityFilm: React.FC<InfinityFilmProps> = ({ images, onImageClick }) => {
   return (
-    <View>
-      <Slide>
+    <div className="view">
+      <ul className="slide">
         {images.concat(images).map((image, index) => (
           <li key={index} onClick={() => onImageClick(index)}>
             <div className="image-container">
-              {/* 필름 형태의 이미지 컨테이너 추가 */}
               <img src={image} alt={`slide-${index}`} />
             </div>
           </li>
         ))}
-      </Slide>
-    </View>
+      </ul>
+    </div>
   );
 };
 
--- src/components/invite/photoGallery/PhotoGallery.tsx
+++ src/components/invite/photoGallery/PhotoGallery.tsx
@@ -1,10 +1,9 @@
-// PhotoGallery.tsx
 import React, { useState, useEffect } from "react";
 import Modal from "react-modal";
 import InfinityFilm from "../../common/infinityFilm/InfinityFilm";
-import { styled } from "styled-components";
 import useEmblaCarousel from "embla-carousel-react";
 import { CUTE_FONT } from "@/constants/constant";
+import "./PhotoGallery.css";
 
 const customStyles = {
   content: {
@@ -24,60 +23,17 @@ const customStyles = {
   },
 };
 
-const DotNav = styled.div`
-  position: absolute;
-  bottom: 60px;
-  left: 50%;
-  transform: translateX(-50%);
-  display: flex;
-`;
-
-const Dot = styled.div<{ $active: boolean }>`
-  width: 10px;
-  height: 10px;
-  margin: 0 5px;
-  border-radius: 50%;
-  background-color: ${(props) => (props.$active ? "white" : "rgba(255, 255, 255, 0.5)")};
-  cursor: pointer;
-`;
-
-const CloseButton = styled.button`
-  position: absolute;
-  bottom: 0px;
-  left: 50%;
-  transform: translateX(-50%);
-  width: 60px;
-  height: 35px;
-  background-color: rgb(145 130 130 / 70%);
-  border-radius: 9999px;
-  font-size: 13px;
-  color: white;
-  border: none;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  cursor: pointer;
-  z-index: 2;
-
-  &:hover {
-    background-color: rgba(255, 255, 255, 1);
-    color: black;
-  }
-`;
-
 type PhotoGalleryProps = {
   images: string[];
 };
 
 function PhotoGallery({ images = [] }: PhotoGalleryProps) {
   const [selectedImageIndex, setSelectedImageIndex] = useState<number | null>(null);
-
   const closeModal = () => {
     setSelectedImageIndex(null);
   };
 
   const [emblaRef, emblaApi] = useEmblaCarousel({ loop: true, startIndex: selectedImageIndex! });
-
   const [selectedDot, setSelectedDot] = useState<number>(0);
 
   useEffect(() => {
@@ -106,27 +62,35 @@ function PhotoGallery({ images = [] }: PhotoGalleryProps) {
       <InfinityFilm images={images} onImageClick={handleImageClick} />
 
       <Modal isOpen={selectedImageIndex !== null} onRequestClose={closeModal} style={customStyles} ariaHideApp={false}>
-        <div className="w-[80vw] max-w-[480px] overflow-hidden">
+        <div className="modal-container">
           <div ref={emblaRef}>
-            <div className="flex ">
+            <div className="carousel-container">
               {images.map((image, index) => (
-                <div key={index} className="flex-none w-full ">
-                  <img src={image} alt={`carousel-image-${index}`} className="w-full  object-contain" />
+                <div key={index} className="carousel-item">
+                  <img src={image} alt={`carousel-image-${index}`} className="carousel-image" />
                 </div>
               ))}
             </div>
           </div>
         </div>
         <div className="absolute h-[100px] left-1/2 transform -translate-x-1/2">
-          <DotNav>
+          <div className="dot-nav">
             {images.map((_, index) => (
-              <Dot key={index} $active={selectedDot === index} onClick={() => handleDotClick(index)} />
+              <div
+                key={index}
+                className={`dot ${selectedDot === index ? "active" : ""}`}
+                onClick={() => handleDotClick(index)}
+              />
             ))}
-          </DotNav>
+          </div>
 
-          <CloseButton onClick={closeModal} style={{ fontFamily: `${CUTE_FONT}`, fontSize: "1.3rem" }}>
+          <button
+            className="close-button"
+            onClick={closeModal}
+            style={{ fontFamily: `${CUTE_FONT}`, fontSize: "1.3rem" }}
+          >
             닫기
-          </CloseButton>
+          </button>
         </div>
       </Modal>
     </>
